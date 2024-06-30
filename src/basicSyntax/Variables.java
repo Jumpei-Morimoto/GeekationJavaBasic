f7dae0b28821677493fdf39a9039e6967012adde
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-06-30 22:25:40 +0900

    変数の実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,19 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 10;
+		double  num2 = 0.5;
+		String string1 ="私の好きな";
+		String string2 = "です。";
+		System.out.println (num1);
+		System.out.println(num2);
+		System.out.println(string1+"整数は"+num1+string2);
+		System.out.println(string1+"少数は"+num2+string2);
+
+
+	}
+
+}
