cd92a16208bbad767d02ac79409aeadb75f76585
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-03 23:12:17 +0900

    比較演算子と論理演算子の実装

---
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -0,0 +1,28 @@
+package basicSyntax;
+
+public class ComparisonLogicalOperators {
+
+    public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    int a = 10;
+    int  b =5;
+    boolean isEqualA =(a > b);
+    System.out.println(isEqualA);
+
+    String tenki ="晴れ";
+    String kion ="暖かい";
+
+    boolean isSunny = (tenki=="晴れ");
+    boolean isWarm =(kion=="暖かい");
+
+    System.out.println(isSunny=true && isWarm==true);
+
+    int x = 5;
+    int y = 6;
+
+    System.out.println(x>0 && y % 2 == 0 );
+
+    boolean hasPermission = false;
+    System.out.println(hasPermission==false);  
+    }
+}
