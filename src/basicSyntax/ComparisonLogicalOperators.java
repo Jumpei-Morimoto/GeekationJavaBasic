f278713b6f335ed50bfb339fc8abfb2f395c4ec6
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-04 22:26:40 +0900

    修正

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -4,25 +4,25 @@ public class ComparisonLogicalOperators {
 
     public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-    int a = 10;
-    int  b =5;
-    boolean isEqualA =(a > b);
-    System.out.println(isEqualA);
+     int a = 10;
+     int  b =5;
+     boolean isEqualA =(a > b);
+     System.out.println(isEqualA);
 
-    String tenki ="晴れ";
-    String kion ="暖かい";
+     String tenki ="晴れ";
+     String kion ="暖かい";
 
-    boolean isSunny = (tenki=="晴れ");
-    boolean isWarm =(kion=="暖かい");
+     boolean isSunny = (tenki=="晴れ");
+     boolean isWarm =(kion=="暖かい");
 
-    System.out.println(isSunny=true && isWarm==true);
+     System.out.println(isSunny==true && isWarm==true);
 
-    int x = 5;
-    int y = 6;
+     int x = 5;
+     int y = 6;
 
-    System.out.println(x>0 && y % 2 == 0 );
+     System.out.println(x >= 0 && y % 2 == 0 );
 
-    boolean hasPermission = false;
-    System.out.println(hasPermission==false);  
+     boolean hasPermission = false;
+     System.out.println(hasPermission==false);  
     }
 }
