52a998a6b8b00358bf887967976994703c624306
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-09 21:16:33 +0900

    修正

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -8,28 +8,29 @@ public class GeekIntroduction {
     }
 
     public static void specialSkill(int num1 , int num2){
-        for(int i = num1 ; num1<=i&&i<=num2;i++){
-          if(num1<1 || num2<1){
-        	System.out.println("num1とnum2は0より大きい値にしてください。");
-            break;
-          }else if(num1>=num2){
+        if(num2 <= num1){
             System.out.println("num2はnum1より大きい値にしてください。");
-            break;
-          }else if(num1>300 || num2>300){
-            System.out.println("num1とnum2は300以下にしてください。");
-            break;
-          }else if(i % 3 == 0 && i % 5 == 0){
-            System.out.println("{対象の値}は3の倍数かつ5の倍数です。");
-            continue;
-          }else if(i % 3 == 0){
-            System.out.println("{対象の値}は3の倍数です。");
-            continue;
-          }else if(i % 5 == 0){
-            System.out.println("{対象の値}は5の倍数です。");
-            continue;
-          }else{
-            System.out.println(i);
-          }
+        }else{
+    	    for(int i = num1 ; i<=num2 ;i++){
+              if(num1<1 || num2<1){
+        	    System.out.println("num1とnum2は0より大きい値にしてください。");
+                break;
+              }else if(num1>300 || num2>300){
+                System.out.println("num1とnum2は300以下にしてください。");
+                break;
+              }else if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("{対象の値}は3の倍数かつ5の倍数です。");
+                continue;
+              }else if(i % 3 == 0){
+                System.out.println("{対象の値}は3の倍数です。");
+                continue;
+              }else if(i % 5 == 0){
+                System.out.println("{対象の値}は5の倍数です。");
+                continue;
+              }else{
+                System.out.println(i);
+              }
+    	    }
         }
     }
 }
