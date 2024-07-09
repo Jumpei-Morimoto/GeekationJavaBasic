73eb779005548d529663df3cf22d41591f4d94fb
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-09 17:05:57 +0900

    クラス課題

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,10 @@
+package basicSyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        GeekIntroduction.greeting(20,170.5);
+        GeekIntroduction.specialSkill(2,10);
+    }
+}
