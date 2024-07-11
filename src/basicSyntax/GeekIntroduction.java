d962d3d244c34cacda9e46f1a96ab33e56345d51
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-11 23:54:55 +0900

    修正３

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -12,22 +12,18 @@ public class GeekIntroduction {
     	    System.out.println("num1とnum2は0より大きい値にしてください。");
         }else if(num2 <= num1){
             System.out.println("num2はnum1より大きい値にしてください。");
+        }else if(num1>300 || num2>300){
+        	System.out.println("num1とnum2は300以下にしてください。"); 
         }else{
-            for(int i = num1 ; i<=num2 ;i++){
-              if(num1<1 || num2<1){
-        	    System.out.println("num1とnum2は0より大きい値にしてください。");
-                break;
-              }else if(num1>300 || num2>300){
-                System.out.println("num1とnum2は300以下にしてください。");
-                break;
-              }else if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("{対象の値}は3の倍数かつ5の倍数です。");
+            for(int i = num1 ; i<=num2 ;i++) {
+              if(i % 3 == 0 && i % 5 == 0){
+                System.out.println(i+"は3の倍数かつ5の倍数です。");
                 continue;
               }else if(i % 3 == 0){
-                System.out.println("{対象の値}は3の倍数です。");
+                System.out.println(i+"は3の倍数です。");
                 continue;
               }else if(i % 5 == 0){
-                System.out.println("{対象の値}は5の倍数です。");
+                System.out.println(i+"は5の倍数です。");
                 continue;
               }else{
                 System.out.println(i);
