344e5b8a035a6ec45f7aea6ba54d8b1849f89286
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-15 04:30:07 +0900

    オブジェクト指向

---
+++ src/objectExercise/Land.java
@@ -0,0 +1,8 @@
+package objectExercise;
+
+public class Land extends Property{
+        Land(String propertyName,String name,String type,int price,String houseLayout){
+        super(propertyName,name,type,price,houseLayout);
+        }
+
+}
