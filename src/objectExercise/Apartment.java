c48e0d93e08aeee8e1e3d0fa5e79d139653de57a
Jumpei-Morimoto <j.morimoto.cingroup2012@gmail.com>
2024-07-16 11:48:26 +0900

    修正

--- src/objectExercise/Apartment.java
+++ src/objectExercise/Apartment.java
@@ -1,7 +1,7 @@
 package objectExercise;
 
 public class Apartment extends Property{
-        Apartment(String propertyName,String name,String type,int price,String houseLayout){
-        super(propertyName,name,type,price,houseLayout);
-        }
+  Apartment(String propertyName,String name,String type,int price,String houseLayout){
+    super(propertyName,name,type,price,houseLayout);
+  }
 }
--- src/objectExercise/Property.java
+++ src/objectExercise/Property.java
@@ -1,58 +1,60 @@
 package objectExercise;
 
 public class Property {
-	private  String propertyName;
-	private String name;
-	private String type;
-	private int price;
-	private  String houseLayout;
+    private  String propertyName;
+    private String name;
+    private String type;
+    private int price;
+    private  String houseLayout;
 
-	Property(String propertyName,String name,String type,int price,String houseLayout){
-	this.propertyName = propertyName;
-	this.name = name;
-	this.type = type;
-	this.price = price;
-	this.houseLayout= houseLayout;
-	}
+    Property(String propertyName,String name,String type,int price,String houseLayout){
+      this.propertyName = propertyName;
+      this.name = name;
+      this.type = type;
+      this.price = price;
+      this.houseLayout= houseLayout;
+    }
 
-	public String getPropertyName (){
-	return this.propertyName;
-	}
-	public String getName (){
-	return this.name;
-	}
-	public String getType (){
-	return this.type;
-	}
-	public int getPrice (){
-	return this.price;
-	}
-	public String getHouseLayout (){
-	return this.houseLayout;
-	}
+    public String getPropertyName (){
+      return this.propertyName;
+    }
+    public String getName (){
+      return this.name;
+    }
+    public String getType (){
+      return this.type;
+    }
+    public int getPrice (){
+      return this.price;
+    }
+    public String getHouseLayout (){
+      return this.houseLayout;
+    }
 
     public void setPropertyName (String propertyName){
-        this.propertyName = propertyName;
-        }
-		public void setName (String name){
-		this.name = name;
-		}
-		public void setType (String type){
-		this.type = type;
-		}
-		public void setPrice (int price){
-		this.price = price;
-		}
-		public void setHouseLayout (String houseLayout){
-		this.houseLayout = houseLayout;
-		}
+      this.propertyName = propertyName;
+    }
+    public void setName (String name){
+      this.name = name;
+    }
+    public void setType (String type){
+      this.type = type;
+    }
+    public void setPrice (int price){
+      this.price = price;
+    }
+    public void setHouseLayout (String houseLayout){
+      this.houseLayout = houseLayout;
+    }
 
-		public void  data(){
-		System.out.println("物件名:"+propertyName);
-		System.out.println("物件所有者名:"+name);
-		System.out.println("物件種別:"+type);
-		System.out.println("物件価格:"+price+"円");
-		System.out.println("間取り:"+houseLayout);
-		}
+    public void  data(){
+      System.out.println("============================="); 
+      System.out.println("物件名:"+propertyName);
+      System.out.println("物件所有者名:"+name);
+      System.out.println("物件種別:"+type);
+      System.out.println("物件価格:"+price+"円");
+      System.out.println("間取り:"+houseLayout);
+      System.out.println("=============================");
+    }
 
 }
